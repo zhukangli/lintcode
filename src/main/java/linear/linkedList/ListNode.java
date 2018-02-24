36a28441de1d99747beadf81c38075740c62fb58
conleyzhu <conleyzhu@kugou.net>
2018-02-24 17:54:26 +0800

    翻转单链表

---
+++ src/main/java/linear/linkedList/ListNode.java
@@ -0,0 +1,13 @@
+package linear.linkedList;
+
+/**
+ * Created by conleyzhu on 2018/2/24.
+ */
+public class ListNode {
+    int val;
+    ListNode next;
+    ListNode(int val) {
+        this.val = val;
+        this.next = null;
+    }
+}
---
+++ src/main/java/linear/linkedList/ReverseLinkedList.java
@@ -0,0 +1,61 @@
+package linear.linkedList;
+
+/**
+ * 翻转一个链表
+ * 样例
+ 给出一个链表1->2->3->null，这个翻转后的链表为3->2->1->null
+ *
+ *
+ * 原理其实就是把node.next 改为 node.prev即可，最后返回prev的指针
+ * Created by conleyzhu on 2018/2/24.
+ */
+public class ReverseLinkedList {
+    /*
+        * @param head: n
+        * @return: The new head of reversed linked list.
+        */
+    public ListNode reverse(ListNode head) {
+        // write your code here
+        ListNode prev = null;
+        while (head != null) {
+            //把当前节点的下一节点存到temp
+            ListNode temp = head.next;
+            //改变下一节点为前节点
+            head.next = prev;
+            //移动prev指针的位置，指示头位置方便返回
+            prev = head;
+            //继续遍历下一个节点
+            head = temp;//可以替换为head = head.next
+        }
+        return prev;
+    }
+
+
+
+
+    public static void main(String[] args) {
+
+        ListNode node1 = new ListNode(1);
+        ListNode node2 = new ListNode(2);
+        ListNode node3 = new ListNode(3);
+        node1.next = node2;
+        node2.next = node3;
+
+        System.out.println("翻转链表前=====================>");
+        ListNode node = node1;
+        while(node.next != null){
+            System.out.print(node.val + ">");
+            node= node.next;
+        }
+        System.out.println("\n翻转链表后=====================>");
+
+        ReverseLinkedList rll = new ReverseLinkedList();
+        ListNode newNode = rll.reverse(node1);
+        while(newNode.next != null) {
+            System.out.print(newNode.val + ">");
+            newNode = newNode.next;
+        }
+
+    }
+
+}
